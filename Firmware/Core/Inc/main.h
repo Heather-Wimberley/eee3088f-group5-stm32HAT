b8ab9489538dc0db69355deae4695492131c9b7f
Heather <wmbhea001@myuct.ac.za>
2022-04-25 10:12:45 +0200

    Add software setup

---
+++ Firmware/Core/Inc/main.h
@@ -0,0 +1,88 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f0xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define B1_Pin GPIO_PIN_0
+#define B1_GPIO_Port GPIOA
+#define USB_on_Pin GPIO_PIN_4
+#define USB_on_GPIO_Port GPIOA
+#define Temp_Active_Pin GPIO_PIN_0
+#define Temp_Active_GPIO_Port GPIOB
+#define Temp_Data_Pin GPIO_PIN_1
+#define Temp_Data_GPIO_Port GPIOB
+#define LD4_Pin GPIO_PIN_8
+#define LD4_GPIO_Port GPIOC
+#define LD3_Pin GPIO_PIN_9
+#define LD3_GPIO_Port GPIOC
+#define SWDIO_Pin GPIO_PIN_13
+#define SWDIO_GPIO_Port GPIOA
+#define SWCLK_Pin GPIO_PIN_14
+#define SWCLK_GPIO_Port GPIOA
+#define Proximity_Active_Pin GPIO_PIN_4
+#define Proximity_Active_GPIO_Port GPIOB
+#define Proximity_Interrupt_Pin GPIO_PIN_5
+#define Proximity_Interrupt_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
