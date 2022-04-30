a81848d95b7befaa616031330583c3faafebf569
Kelvin Yankey <kelvinyankey6@gmail.com>
2022-04-30 11:45:31 +0200

    Added Temperature Sensing API.

---
+++ Firmware/Core/Src/Sensors/temp.h
@@ -0,0 +1,29 @@
+#ifndef TEMP_SENSOR
+#define TEMP_SENSOR
+#include "stm32f0xx_hal.h"
+
+
+extern ADC_HandleTypeDef *ADCState;
+extern char SensorState;
+
+
+
+
+void TempSensor_Start(ADC_HandleTypeDef *hadc, );
+
+void TempSensor_Test(void);
+
+float TempSensor_GetMeasurement(int maxTime);
+
+char TempSensor_HasStarted(void);
+
+void TempSensor_Stop(void);
+
+void EnsureHandleValid(void);
+
+
+
+
+
+
+#endif
